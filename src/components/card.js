efeafc37f104e1e55017e44982c378ce11734eba
Caleb Isaacson <agentisaacson@gmail.com>
2018-06-13 09:56:14 -0600

    built first class component for inputs container, and built an input functional component to render custom inputs

--- src/components/card.js
+++ src/components/card.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import Input from './input';
+
 class Card extends Component {
     render() {
         return (
             <div className="card">
-                this is where the inputs go
+                { Input('Super Cool Title') }
+                { Input('Second Title') }
             </div> 
         )
     }
