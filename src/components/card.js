6a6aeb0b89fb1964a0cd291661de8505d1aa6b38
Caleb Isaacson <agentisaacson@gmail.com>
2018-06-14 14:07:06 -0600

    added generate button and got form to render/unrender

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Input from './input';
+import Content from './content';
 
 class Card extends Component {
 
@@ -24,15 +25,22 @@ class Card extends Component {
             nounThree: '',
             celebFour: '',
             adjectiveFive: '',
+            contentVisible: false
         }
 
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
     }
 
     handleInputChange(event) {
         this.setState({ [event.target.name]: event.target.value })
-        console.log(this.state);
     }
+
+    handleFormSubmit(event) {
+        event.preventDefault()
+        this.setState({ contentVisible: !this.state.contentVisible })
+    }
+
 
     render() {
 
@@ -60,11 +68,18 @@ class Card extends Component {
         ]
 
         return (
-            <div className="card">
+            <form onSubmit={this.handleFormSubmit} className="card">
+                <div className="card__inputs">
                 {
                     inputData.map(data => Input((data), this.handleInputChange ))
                 }
-            </div> 
+                </div>
+                
+                <button type="submit">{!this.state.contentVisible ? 'Generate Mad lib' : 'Clear Form'}</button>
+                {
+                    this.state.contentVisible ? <Content data={ this.state }/> : ''
+                }
+            </form> 
         )
     }
 }
